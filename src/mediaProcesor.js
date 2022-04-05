a49d53d515b85876ac4e45e9afb8cc23c3cefdc3
Ricardo Recaredo <rrecaredo@twilio.com>
2022-04-05 06:14:54 -0700

    subsample buffers in chunks of 32

--- src/mediaProcesor.js
+++ src/mediaProcesor.js
@@ -2,33 +2,52 @@ let mediaRecorders = [];
 let bufferMap = [];
 let interval = null;
 
-export const startMediaProcessors = async (streams = []) => {
-    mediaRecorders = streams.map(stream => new MediaRecorder(stream));
+export async function startMediaProcessors(streams = []) {
+    mediaRecorders = streams.map(stream => new MediaRecorder(stream, { mimeType: "audio/webm; codecs=pcm" }));
     bufferMap = Array(mediaRecorders.length).fill([]);
 
     mediaRecorders.forEach((mr, i) => {
-        mr.addEventListener("dataavailable", event => {
+        mr.addEventListener("dataavailable", async (event) => {
             if (bufferMap[i]) {
                 const blob = event.data;
-                bufferMap[i].push(blob);
+                const buffer = await blob.arrayBuffer();
+                const typedArray = new Uint8Array(buffer);
+                bufferMap[i] = [...bufferMap[i], ...typedArray];
             }
         });
 
         mr.start(50);
     });
 
-    window.getBuffer = () => console.log(bufferMap);
+    startBufferingIntervals();
+
+    window.getBuffer = () => {
+        const buffersamples = getBufferSamples();
+        console.log({ buffersamples });
+    };
 };
 
-export const stoptMediaProcessors = () => {
+export function getBufferSamples() {
+    const buffersLength = bufferMap.map(({ length }) => length - length % 32);
+    const minLength = Math.min(...buffersLength);
+
+    return bufferMap
+        .map(buffer => buffer.splice(0, minLength))
+        .map(buffer => new Int32Array(buffer));
+};
+
+export function stoptMediaProcessors() {
     mediaRecorders.forEach((mr, i) => {
         mr.stop();
         bufferMap[i] = null;
     });
+
+    interval.stop();
 };
 
-export const startBufferingIntervals = (mediaRecorders = []) => {
+export function startBufferingIntervals() {
     interval = setInterval(() => {
-        //
+        const bufferSamples = getBufferSamples();
+        console.log({ bufferSamples });
     }, 100);
 };
